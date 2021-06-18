9be5b8af030aee3cda37e6e79bdf8eceb7179981
Nuno Lopes <nuno.lopes@ist.utl.pt>
2021-06-18 19:47:38 +0100

    type inference

--- scripts/infer_types.cpp
+++ scripts/infer_types.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2021-present The Torchy Authors.
 // Distributed under the MIT license that can be found in the LICENSE file.
 
+#include <ATen/core/List.h>
 #include <ATen/NativeFunctions.h>
 #include <ATen/RedispatchFunctions.h>
 #include <array>
@@ -28,45 +29,95 @@ ScalarType promoted_type_trail() {
   return ty;
 }
 
-void call(const char *name, function<Tensor()> fn) {
-  cout << name << '(';
-  bool first = true;
-  for (auto ty : type_trail) {
-    if (!first)
-      cout << ", ";
-    cout << ty;
-    first = false;
-  }
-  cout << ") -> ";
-  auto promoted_ty = promoted_type_trail();
-  try {
-    auto dtype = typeMetaToScalarType(fn().dtype());
-    cout << dtype;
-    if (dtype != promoted_ty)
-      cout << " [not equal to promoted type: " << promoted_ty << ']';
-    cout << '\n';
+struct C {
+  static void call(const char *name, function<Tensor()> fn) {
+    cout << name << '(';
+    bool first = true;
+    for (auto ty : type_trail) {
+      if (!first)
+        cout << ", ";
+      cout << ty;
+      first = false;
+    }
+    cout << ") -> ";
+    auto promoted_ty = promoted_type_trail();
+    try {
+      auto dtype = typeMetaToScalarType(fn().dtype());
+      cout << dtype;
+      if (dtype != promoted_ty)
+        cout << " [not equal to promoted type: " << promoted_ty << ']';
+      cout << '\n';
 
-  } catch (const c10::Error &) {
-    cout << "Exception! [promoted type: " << promoted_ty << "]\n";
+    } catch (const c10::Error &) {
+      cout << "Exception! [promoted type: " << promoted_ty << "]\n";
+    }
   }
-}
 
-template <typename... Tail>
-void call(const char *name, function<Tensor(const Tensor&, Tail...)> fn) {
-  for (auto ty : types) {
-    if (isQIntType(ty))
-      continue;
-    type_trail.emplace_back(ty);
-    call(name, function<Tensor(Tail&&...)>{[=](Tail&&... args) -> Tensor {
-      return fn(native::empty_cpu({1}, ty), forward<Tail>(args)...);
-    }});
-    type_trail.pop_back();
+  template <typename... Tail>
+  static void call(const char *name, function<Tensor(Tensor&, Tail...)> fn) {
+    for (auto ty : types) {
+      if (isQIntType(ty))
+        continue;
+      type_trail.emplace_back(ty);
+      call(name, function<Tensor(Tail&&...)>{[=](Tail&&... args) -> Tensor {
+        auto t = native::empty_cpu({1}, ty);
+        return fn(t, forward<Tail>(args)...);
+      }});
+      type_trail.pop_back();
+    }
   }
-}
+
+  template <typename... Tail>
+  static void call(const char *name, function<Tensor(TensorList&, Tail...)> fn) {
+    for (auto ty1 : types) {
+      if (isQIntType(ty1))
+        continue;
+      type_trail.emplace_back(ty1);
+
+      for (auto ty2 : types) {
+        if (isQIntType(ty2))
+          continue;
+        type_trail.emplace_back(ty2);
+        call(name, function<Tensor(Tail&&...)>{[=](Tail&&... args) -> Tensor {
+          Tensor ts[2] = { native::empty_cpu({1}, ty1),
+                          native::empty_cpu({1}, ty2) };
+          ArrayRef<Tensor> aref(ts);
+          return fn(aref, forward<Tail>(args)...);
+        }});
+        type_trail.pop_back();
+      }
+      type_trail.pop_back();
+    }
+  }
+
+  template <typename... Tail>
+  static void call(const char *name,
+                   function<Tensor(List<optional<Tensor>>&, Tail...)> fn) {
+    for (auto ty1 : types) {
+      if (isQIntType(ty1))
+        continue;
+      type_trail.emplace_back(ty1);
+
+      for (auto ty2 : types) {
+        if (isQIntType(ty2))
+          continue;
+        type_trail.emplace_back(ty2);
+        call(name, function<Tensor(Tail&&...)>{[=](Tail&&... args) -> Tensor {
+          List<optional<Tensor>> list({ native::empty_cpu({1}, ty1),
+                                        native::empty_cpu({1}, ty2) });
+          return fn(list, forward<Tail>(args)...);
+        }});
+        type_trail.pop_back();
+      }
+      type_trail.pop_back();
+    }
+  }
+
+};
 
 }
 
 int main() {
-  call("add", function<Tensor(const Tensor &, const Tensor &)>{[](const Tensor &t0, const Tensor &t1) { return redispatch::add(DispatchKeySet(DispatchKey::CPU), t0, t1, Scalar(0)); }});
+#include "call_pytorch_fns.h"
   return 0;
 }
--- scripts/infer_types.py
+++ scripts/infer_types.py
@@ -15,10 +15,36 @@ native_functions = parse_native_yaml(yaml_path)
 
 def mk_arg(arg, tensors):
   type = arg.type.cpp_type()
+  if 'TensorList' in type:
+    t = f't{len(tensors)}'
+    tensors.append(('TensorList &', t))
+    return t
+  if type == 'const c10::List<c10::optional<at::Tensor>> &':
+    t = f't{len(tensors)}'
+    tensors.append(('List<optional<Tensor>> &', t))
+    return t
   if 'Tensor' in type:
     t = f't{len(tensors)}'
-    tensors.append(t)
+    tensors.append(('Tensor &', t))
     return t
+  if type == 'const at::Scalar &' or type == 'const c10::optional<at::Scalar> &':
+    return 'Scalar(0)'
+  if type == 'c10::string_view':
+    return '"foo"'
+  if type == 'int64_t':
+    return '1'
+  if type == 'double':
+    return '1.0'
+  if type == 'bool':
+    return 'false'
+  if type == 'at::IntArrayRef':
+    return 'IntArrayRef{0}'
+  if 'optional<' in type:
+    return 'nullopt'
+  if type == 'at::Dimname':
+    return 'Dimname::wildcard()'
+  if type == 'at::DimnameList':
+    return '{Dimname::wildcard()}'
   return '{}'
 
 
@@ -32,14 +58,19 @@ def gen(fn):
   tensors = []
   args = [mk_arg(arg, tensors) for arg in args]
 
-  fn = f'auto result = at::redispatch::{sig.name()}({", ".join(args)})'
+  if not tensors:
+    return f'// skip {fn.func.name}'
 
-  print(fn)
-  exit()
+  key = 'DispatchKeySet(DispatchKey::CPU)'
+  types = ', '.join(ty for ty,name in tensors)
+  types_names = ', '.join(f'{ty} {name}' for ty,name in tensors)
+  return f'C::call("{fn.func.name}", function<Tensor({types})>{{[]({types_names}) {{' +\
+         f' return at::redispatch::{sig.name()}({key}, {", ".join(args)}); }}}});'
 
 
+fd = open('scripts/call_pytorch_fns.h', 'w')
 for fn in native_functions.native_functions:
   if len(fn.func.returns) != 1 or str(fn.func.returns[0].type) != 'Tensor':
     continue
 
-  gen(fn)
+  print(gen(fn), file=fd)
