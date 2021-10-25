fa0f41e6179edf529cee60bb897c07bddbf0bf99
Nuno Lopes <nuno.lopes@ist.utl.pt>
2021-10-25 19:06:22 +0100

    experiment with fusion

--- benchmarks/testdriver.py
+++ benchmarks/testdriver.py
@@ -14,5 +14,17 @@ for arg in sys.argv[1:]:
     if not torch.cuda.is_available():
       print('UNSUPPORTED: CUDA is not available')
       exit(0x42)
+  elif arg == '--fuser-nnc':
+    torch._C._jit_override_can_fuse_on_cpu(True)
+    torch._C._jit_override_can_fuse_on_gpu(True)
+    torch._C._jit_set_texpr_parallel_cpu_enabled(True)
+  elif arg == '--nvfuser':
+    #os.environ['PYTORCH_CUDA_FUSER_DISABLE_FALLBACK'] = '1'
+    #os.environ['PYTORCH_CUDA_FUSER_DISABLE_FMA'] = '1'
+    #os.environ['PYTORCH_CUDA_FUSER_JIT_OPT_LEVEL'] = '0'
+    torch._C._jit_override_can_fuse_on_cpu(False)
+    torch._C._jit_override_can_fuse_on_gpu(False)
+    torch._C._jit_set_texpr_fuser_enabled(False)
+    torch._C._jit_set_nvfuser_enabled(True)
   else:
     print(f'Unknown arg: {arg}')
